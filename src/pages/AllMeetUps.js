74435b07cbfccc69246daa7fd91f21e6735ce9c7
Sanish Thapa <thapasanish73@gmail.com>
2023-01-12 21:49:09 +0545

    errror mathi error

---
+++ src/pages/AllMeetUps.js
@@ -0,0 +1,7 @@
+function AllMeetUpsPage(){
+    return <div>
+        <h1>All Meet up page</h1>
+    </div>
+}
+
+export default AllMeetUpsPage;
---
+++ src/pages/NewMeetUp.js
@@ -0,0 +1,7 @@
+function NewMeetUpsPage(){
+    return <div>
+        <h1>NewMeetUp page</h1>
+    </div>
+}
+
+export default NewMeetUpsPage;
